91dadaba32f1aa3ec162cfb7bb865dad5ca46a3a
Aleksandra Kulbaka <awkulbaka1@sheffield.ac.uk>
2019-11-02 21:30:09 +0000

    Add css to log in page

--- src/components/create-user.component.js
+++ src/components/create-user.component.js
@@ -1,21 +1,27 @@
 import React, { Component } from 'react';
+import'./create-user.css'
 
 export default class CreateUser extends Component {
     render() {
         return (
-        <div className="container"> 
-                <form>
-                    <div class="form-group">
-                    <label for="exampleInputEmail1">Email address</label>
-                    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email" style={{width: '15em'}} required></input>
-                    </div>
-                    <div class="form-group">
-                        <label for="exampleInputPassword1">Password</label>
-                        <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password" style={{width: '15em'}} required></input>
-                    </div>                
-                    <button type="submit" class="btn btn-outline-secondary">Submit</button>
-                </form>
-        </div>
+            <div>
+                <div className="container"> 
+                        <form>
+                            <div class="form-group">
+                            <label for="exampleInputEmail1">Email address</label>
+                            <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email" style={{width: '15em'}} required></input>
+                            </div>
+                            <div class="form-group">
+                                <label for="exampleInputPassword1">Password</label>
+                                <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password" style={{width: '15em'}} required></input>
+                            </div>                
+                            <button type="submit" class="btn btn-outline-secondary">Submit</button>
+                        </form>
+                </div>
+                <div className="picture">
+                    <p>picture</p>
+                </div>
+            </div>
         )
     }
 }
