e3f78f20c9f2f7db7d543a1c05907ed39e52d724
Aleksandra Kulbaka <awkulbaka1@sheffield.ac.uk>
2019-11-02 21:19:34 +0000

    Add the sign up page

--- src/components/create-user.component.js
+++ src/components/create-user.component.js
@@ -3,9 +3,19 @@ import React, { Component } from 'react';
 export default class CreateUser extends Component {
     render() {
         return (
-            <div>
-                <p>You are on the Create User component!</p>
-            </div>
+        <div className="container"> 
+                <form>
+                    <div class="form-group">
+                    <label for="exampleInputEmail1">Email address</label>
+                    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email" style={{width: '15em'}} required></input>
+                    </div>
+                    <div class="form-group">
+                        <label for="exampleInputPassword1">Password</label>
+                        <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password" style={{width: '15em'}} required></input>
+                    </div>                
+                    <button type="submit" class="btn btn-outline-secondary">Submit</button>
+                </form>
+        </div>
         )
     }
 }
