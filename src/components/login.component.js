66326bd42362696026b86e3151837d4accb2a907
Aleksandra Kulbaka <awkulbaka1@sheffield.ac.uk>
2019-11-03 00:28:49 +0000

    Add connections between log in form and database

--- src/components/login.component.js
+++ src/components/login.component.js
@@ -1,18 +1,65 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 import './user-form.css'
 
 export default class Login extends Component {
+    constructor(props) {
+        super(props);
+    
+        this.onChangeUsername = this.onChangeUsername.bind(this);
+        this.onChangePassword = this.onChangePassword.bind(this);
+        this.onSubmit = this.onSubmit.bind(this);
+    
+        this.state = {
+          username: '',
+          password: ''
+        }
+    }
+
+    onChangeUsername(e) {
+        this.setState({
+          username: e.target.value
+        })
+      }
+
+      onChangePassword(e) {
+        this.setState({
+          password: e.target.value
+        })
+      }
+
+    onSubmit(e) {
+        e.preventDefault();
+    
+        const user = {
+            username: this.state.username,
+            password: this.state.password
+        }
+    
+        console.log(user);
+    
+        axios.post('http://localhost:5000/users/add', user)
+            .then(res => console.log(res.data));
+    
+        this.setState({
+            username: '',
+            password: ''
+        })
+    }
+
     render() {
         return (
             <div>
                 <div className="container"> 
-                        <form>
+                        <form onSubmit={this.onSubmit}>
                             <div class="form-group">
                                 <label for="InputUsername">Username</label>
                                 <input
                                     type="text"
                                     class="form-control"
                                     id="InputUsername"
+                                    value={this.state.username}
+                                    onChange={this.onChangeUsername}
                                     placeholder="Enter username"
                                     style={{ width: '15em' }}
                                     required
@@ -20,9 +67,16 @@ export default class Login extends Component {
                             </div>
                             <div class="form-group">
                                 <label for="exampleInputPassword1">Password</label>
-                                <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password" style={{width: '15em'}} required></input>
+                                <input type="password"
+                                    class="form-control"
+                                    id="exampleInputPassword1"
+                                    value={this.state.password}
+                                    onChange={this.onChangePassword}
+                                    placeholder="Password"
+                                    style={{width: '15em'}}
+                                    required></input>
                             </div>                
-                            <button type="submit" class="btn btn-outline-secondary">Submit</button>
+                            <button type="submit" class="btn btn-outline-secondary" value="Create User">Submit</button>
                         </form>
                 </div>
                 <div className="picture">
